8603c6c0e2485eb8da78c89e392478dcd74e9c7e
LenharesY2K <lenharespedro@gmail.com>
2025-10-06 12:04:49 -0300

    KimmyIA!

---
+++ particle.js
@@ -0,0 +1,16 @@
+const particlesContainer = document.getElementById('particles');
+    const particleCount = 40;
+
+    for  (let i = 0; i < particleCount; i++) {
+      const particle = document.createElement('div');
+      particle.className = 'particle';
+
+      const size = Math.random() * 4 + 2;
+      particle.style.width = size + 'px';
+      particle.style.height = size + 'px';
+      particle.style.left = Math.random() * 100 + '%';
+      particle.style.animationDelay = Math.random() * 15 + 's';
+      particle.style.animationDuration = (Math.random() * 10 + 10) + 's';
+
+      particlesContainer.appendChild(particle);
+    }
